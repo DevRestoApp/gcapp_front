c333beca1717d5b3d41bf56d5d9033a03f8eed53
qoqosyk <asdovicasd@gmail.com>
2025-09-07 13:36:42 +0800

    feat: context provider wrapper

--- src/contexts/AppProviders.tsx
+++ src/contexts/AppProviders.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { AuthProvider } from "./AuthContext";
 
-// ADD ur providers here
 export function AppProviders({ children }: { children: ReactNode }) {
     return <AuthProvider>{children}</AuthProvider>;
 }
