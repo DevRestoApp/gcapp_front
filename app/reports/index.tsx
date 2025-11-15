0875db012d181c6f483d0f47f4485d7a1ad342ad
qoqosyk <asdovicasd@gmail.com>
2025-11-15 16:12:59 +0800

    feat: add go back in report index

--- app/reports/index.tsx
+++ app/reports/index.tsx
@@ -75,6 +75,7 @@ export default function AnalyticsScreen() {
                     date={filters.date}
                     period={filters.period}
                     location={filters.organization_id}
+                    onBack={() => router.push("/ceo")}
                     onDateChange={setDate}
                     onPeriodChange={setPeriod}
                     onLocationChange={setLocation}
@@ -93,6 +94,7 @@ export default function AnalyticsScreen() {
                     date={filters.date}
                     period={filters.period}
                     location={filters.organization_id}
+                    onBack={() => router.push("/ceo")}
                     onDateChange={setDate}
                     onPeriodChange={setPeriod}
                     onLocationChange={setLocation}
